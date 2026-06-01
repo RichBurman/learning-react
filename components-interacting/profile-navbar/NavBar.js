3ca4c4b584061967ceee65a1f39811981a311647
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-06-01 19:12:51 +0100

    Add NavBar and ProfilePage components for navigation and user profile display

---
+++ components-interacting/profile-navbar/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+    const pages = ['home', 'blog', 'pics', 'bio', 'art', 'shop', 'about', 'contact'];
+    const navLinks = pages.map(page => {
+      return (
+        <a href={'/' + page}>
+           &nbsp;{page}
+        </a>
+      )
+    });
+
+    return <nav>{navLinks}</nav>;
+}
+export default NavBar;
