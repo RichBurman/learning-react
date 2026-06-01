6c16fc5d1904a6dab98365bf36d8535eb6fbc025
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-06-01 15:16:50 +0100

    Add initial HTML and JavaScript setup for React application

---
+++ react-components/first-react-app/functions/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+function MyComponent() {
+  return <h1>Hello world</h1>;
+}
---
+++ react-components/first-react-app/functions/index.js
@@ -0,0 +1,4 @@
+import { createRoot } from 'react-dom/client';
+import MyComponent from './App';
+
+const root = createRoot(document.getElementById('app')).render(<MyComponent />);
