bde39d12351253731f55a248c68c20f9403ba59d
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-06-02 19:21:18 +0100

    Add Musical component with state management for title, actors, and locations

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -219,6 +219,28 @@ export default function EditProfile() {
   );
 }
 
+// Seperate Hook for Seperate States
+
+import React, { useState } from "react";
+
+function Musical() {
+    const [title, setTitle] = useState ("Best Musical Ever");
+    const [actors, setActors] = useState (["George Wilson", "Tim Hughes", "Larry Clements"]);
+    const [locations, setLocations] = useState({
+      Chicago: {
+        dates: ["1/1", "2/2"], 
+        address: "chicago theater"}, 
+      SanFrancisco: {
+        dates: ["5/2"], 
+        address: "sf theater"
+      }
+    });
+   }
+
+function MusicalRefactored() {
+
+}
+
 
 
 
