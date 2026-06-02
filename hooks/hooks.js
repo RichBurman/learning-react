425e7a5e49f8e746787af87d2c49b1d24cb86ee6
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-06-02 15:12:46 +0100

    Add PhoneNumber component with validation for phone input

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -68,3 +68,34 @@ export default function ColorPicker() {
 }
 
 
+// Use State Setter Outside of JSX
+
+import React, { useState } from 'react';
+
+// regex to match numbers between 1 and 10 digits long
+const validPhoneNumber = /^\d{1,10}$/;
+
+export default function PhoneNumber() {
+  // declare current state and state setter 
+  const [phone, setPhone] = useState('101');
+  const handleChange = ({ target })=> {
+    const newPhone = target.value;
+    const isValid = validPhoneNumber.test(newPhone);
+    if (isValid) {
+        // update state 
+        setPhone(newPhone);
+    }
+    // just ignore the event, when new value is invalid
+  };
+
+  return (
+    <div className='phone'>
+      <label for='phone-input'>Phone: </label>
+      <input id='phone-input' value={phone} onChange={handleChange} />
+    </div>
+  );
+}
+
+
+
+
