3f8fe4e8336e8dd1fc98d4da889118674242bacc
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-06-02 09:06:44 +0100

    Refactor Button and Talker components to use consistent onClick prop for event handling

--- props/event-handler-in-function/Button.js
+++ props/event-handler-in-function/Button.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function Button(props) {
-  return (
-    <button onClick={props.talk}>
-      Click me!
-    </button>
-  );
+    return (
+      <button onClick={props.onClick}>
+        Click me!
+      </button>
+    );
 }
 
 export default Button;
