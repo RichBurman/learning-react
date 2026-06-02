288d366496cf8b3da8d36b648b381ea568ca356b
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-06-02 08:54:03 +0100

    Add Talker component with speech alert functionality

---
+++ props/event-handler-in-function/Talker.js
@@ -0,0 +1,14 @@
+import Button from './Button';
+
+function Talker() {
+  function talk() {
+    let speech = '';
+    for (let i = 0; i < 10000; i++) {
+      speech += 'blah ';
+    }
+    alert(speech);
+	}
+  return <Button />;
+}
+
+export default Talker;
