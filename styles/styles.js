976c5f8336fcefb2789d3d123e74f443ee39254f
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-06-03 14:33:05 +0100

    Add styles.js file with inline styles, object variable styles, and CSS Modules integration

---
+++ styles/styles.js
@@ -0,0 +1,10 @@
+// Inline styles
+<h1 style={{color: "red"}}> Hello, World! </h1>
+
+// Object variables style
+const myStyle = { color: "red" }
+<h1 style={myStyle}> Hello, World! </h1>
+
+// CSS Modules
+import styles from './styles/AttentionGrabber.module.css';
+<div className={styles.divStyle}></div>
